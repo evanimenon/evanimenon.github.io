5589fe7744c52a90d01ca831765d03c5cacfc07d
Evani Menon <evanimenon@gmail.com>
2024-02-09 12:23:34 +0530

    c7

--- app.js
+++ app.js
@@ -16,3 +16,12 @@ if (navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+
+/* Remove menu mobile */
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
