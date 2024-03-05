65f0bc4a7e3929a3a8e7000c125fb594a9f3822b
Evani Menon <evanimenon@gmail.com>
2024-03-05 23:51:29 +0530

    c13

--- app.js
+++ app.js
@@ -129,3 +129,14 @@ themeButton.addEventListener('click', () => {
 })
 
 /* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true // Animations repeat
+})
+
+sr.reveal('.home__perfil, .about__image, .contact__mail', {origin: 'right'})
+sr.reveal('.home__name, .home__info, .about__container, .section__title-1, .about__info, .contact__social, .contact__data', { origin: 'left'})
+sr.reveal('.projects__card', {interval: 100})
