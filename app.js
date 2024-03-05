0ca5c77b58d4b6cd4c5236ab9d234bc1ce4a95b7
Evani Menon <evanimenon@gmail.com>
2024-03-05 20:51:26 +0530

    c11

--- app.js
+++ app.js
@@ -34,3 +34,98 @@ const shadowHeader = () =>{
 }
 
 window.addEventListener('scroll', shadowHeader)
+
+/* EMAIL JS */
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_kbnpjm','template_8ta5ygc','#contact-form','FKlcLUbZedV_wNlJU')
+    .then(() =>{
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅';
+
+        // Remove message after 5 seconds
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000);
+
+        // Clear input fields
+        contactForm.reset();
+
+    }, () =>{
+        // Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+/* SHOW SCROLL UP */
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    //when scroll is higher than 350 vieewport height, add show-scroll class to the a tag with scrollup class
+    this.scrollY >=350 ? scrollUp.classList.add('show-scroll')
+                        :scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]');
+const navLinks = document.querySelectorAll('.nav__link');
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach((current, index) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            // Add active-link class to the corresponding navigation link
+            navLinks[index].classList.add('active-link');
+        } else {
+            // Remove active-link class from other navigation links
+            navLinks[index].classList.remove('active-link');
+        }
+    });
+};
+
+window.addEventListener('scroll', scrollActive);
+
+/* DARK LIGHT THEME */
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// Obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// Validate if the user previously chose a topic
+if (selectedTheme) {
+    //If the validation is fulfilled, to know if Dark is activated or deactivated
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+//Activate / deactivates the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark/ icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // Saving the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurentIcon())
+})
+
+/* SCROLL REVEAL ANIMATION */
